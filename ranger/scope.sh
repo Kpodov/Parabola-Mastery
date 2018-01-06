7953fe9353462f787616fd2912f5b3c35ebb5b63
kpodovzky <josue@kpodovzky.org>
2018-01-06 23:23:04 +0000

    fix scope

--- ranger/scope.sh
+++ ranger/scope.sh
@@ -63,6 +63,7 @@ if [ "$preview_images" = "True" ]; then
     esac
 fi
 
+
 case "$extension" in
     # Archive extensions:
     a|ace|alz|arc|arj|bz|bz2|cab|cpio|deb|gz|jar|lha|lz|lzh|lzma|lzo|\
@@ -94,6 +95,7 @@ case "$extension" in
         try elinks -dump "$path" && { dump | trim | fmt -s -w $width; exit 4; }
         ;; # fall back to highlight/cat if the text browsers fail
 esac
+
 
 case "$mimetype" in
     # Syntax highlight for text files:
